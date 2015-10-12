89deee2fa4489eaceeb12876d3ce7608a3381c81
JonasRing <jonas_ring@yahoo.com>
2015-10-12 15:03:07 +0200

    Databas  + Entity framework
    Gjorde class för highscore och installerade Entity framework 6.1.3

---
+++ 5-irad/Models/Highscore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace _5_irad.Models
+{
+    public class Highscore
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Wins { get; set; }
+        public int Points { get; set; }
+    }
+
+    public class HighscoreDBContext : DbContext
+    {
+        public DbSet<Highscore> Highscores { get; set; }
+    }
+}
